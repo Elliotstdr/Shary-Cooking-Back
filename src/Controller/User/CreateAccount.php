754857ad06d839d0e602e7178a018b0d910c5d5a
Elliot Saint-andré <elliotstdr@macbook-air-de-elliot-2.home>
2023-06-01 14:45:17 +0200

    secret key

--- src/Controller/User/CreateAccount.php
+++ src/Controller/User/CreateAccount.php
@@ -2,34 +2,45 @@
 
 namespace App\Controller\User;
 
+use App\Dto\CreateAccountDto;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CreateAccount extends AbstractController
 {
-  public function __construct()
-  {
+  public function __construct(
+    private EntityManagerInterface $em,
+    private UserRepository $ur,
+  ) {
   }
 
-  public function __invoke(User $data, EntityManagerInterface $em, UserRepository $ur)
+  public function __invoke(User $data, SerializerInterface $serializer, Request $request)
   {
+    $accountDto = $serializer->deserialize($request->getContent(), CreateAccountDto::class, 'json');
+
     $factory = new PasswordHasherFactory([
       'common' => ['algorithm' => 'bcrypt'],
     ]);
     $passwordHasher = $factory->getPasswordHasher('common');
 
-    if ($ur->findOneBy(['email' => $data->getEmail()])) {
+    if (!$passwordHasher->verify($this->getParameter('secretKey'), $accountDto->secretKey)) {
+      throw new Exception('La clef secrète que vous avez renseigné est incorrecte.');
+    }
+
+    if ($this->ur->findOneBy(['email' => $data->getEmail()])) {
       throw new Exception('Cette adresse email est déjà utilisée pour un autre compte');
     }
     $data->setPassword($passwordHasher->hash($data->getPassword()));
-    $em->persist($data);
-    $em->flush();
+    $this->em->persist($data);
+    $this->em->flush();
 
-    $returnUser = $ur->findOneBy(['email' => $data->getEmail()]);
+    $returnUser = $this->ur->findOneBy(['email' => $data->getEmail()]);
     return [
       'id' => $returnUser->getId(),
       'name' => $returnUser->getName(),
