104390a1be01f595aad69c96d9b96df40f3ad982
Elliot Saint-andré <elliotstdr@macbook-air-de-elliot-2.home>
2023-06-08 23:52:12 +0200

    Remove from favorite

--- src/Controller/Recipe/SaveRecipe.php
+++ src/Controller/Recipe/SaveRecipe.php
@@ -2,21 +2,33 @@
 
 namespace App\Controller\Recipe;
 
+use App\Dto\SaveRecipeDto;
 use App\Repository\RecipeRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class SaveRecipe extends AbstractController
 {
-  public function saveRecipe($userId, $recipeId, UserRepository $ur, RecipeRepository $rr, EntityManagerInterface $em)
+  public function saveRecipe(Request $request, $userId, $recipeId, UserRepository $ur, RecipeRepository $rr, EntityManagerInterface $em, SerializerInterface $serializer)
   {
+    $recetteCreateDto = $serializer->deserialize($request->getContent(), SaveRecipeDto::class, 'json');
     $recipe = $rr->find($recipeId);
     $user = $ur->find($userId);
-    $user->addSavedRecipe($recipe);
-    $em->persist($user);
-    $em->flush();
+
+    if ($recetteCreateDto->action === "add") {
+      $user->addSavedRecipe($recipe);
+      $em->persist($user);
+      $em->flush();
+    }
+
+    if ($recetteCreateDto->action === "delete") {
+      $user->removeSavedRecipe($recipe);
+      $em->flush();
+    }
 
     return new JsonResponse("Success");
   }
---
+++ src/Dto/SaveRecipeDto.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Dto;
+
+class SaveRecipeDto
+{
+  public $action;
+}
