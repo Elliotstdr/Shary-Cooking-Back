4f03295848944afecc27d50636568c1ac8131dc7
Elliot Saint-André <elliot.standre@gmail.com>
2023-06-28 23:25:43 +0200

    [ADD] Reduce image size on load

--- src/Controller/Recipe/PostImageRecipe.php
+++ src/Controller/Recipe/PostImageRecipe.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PostImageRecipe extends AbstractController
 {
@@ -22,8 +23,21 @@ class PostImageRecipe extends AbstractController
     $recipe = $rr->find($id);
 
     $fileData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64File));
-    $fileName = $jsonData['fileName'];
-    file_put_contents($this->getParameter('kernel.project_dir') . '/public/media/' . $fileName, $fileData);
+    $fileName = uniqid('', true) . '.jpg';
+    $filePath = $this->getParameter('kernel.project_dir') . '/public/media/' . $fileName;
+
+    $GBPicture = Image::make($fileData);
+    $pictureWidth = $GBPicture->width();
+    $pictureHeight = $GBPicture->height();
+
+    $divider = $pictureWidth / 1000;
+
+    if ($divider > 1) {
+      $GBPicture->resize($pictureWidth / $divider, $pictureHeight / $divider);
+      $GBPicture->save($filePath);
+    } else {
+      file_put_contents($filePath, $fileData);
+    }
 
     $recipe->setImageUrl('/media/' . $fileName);
     $em->persist(($recipe));
