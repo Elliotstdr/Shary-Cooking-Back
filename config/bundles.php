7b04b68c308abef67e88994529e22ce00bf95cf6
Elliot Saint-André <elliot.standre@gmail.com>
2024-03-07 18:24:27 +0100

    [ADD] Refresh Token

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
